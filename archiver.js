0ea35f4cb604317b81a339cf85fbad807e855272
Cédric JUNG <cedced19@gmail.com>
2017-07-24 11:24:55 +0200

    Add saving attachments

--- archiver.js
+++ archiver.js
@@ -4,12 +4,14 @@
 var got = require('got'),
     schedule = require('node-schedule'),
     fs = require('fs'),
+    path = require('path'),
     log = require('./lib/log'),
+    downloadAttachments = require('./lib/download-attachments'),
     isUrl = require('is-url'),
     moment = require('moment'),
     isThere =  require('is-there');
 
-if (!isThere(__dirname + '/config.json')) {
+if (!isThere(path.resolve(__dirname, './config.json'))) {
   log('You must create config.json file!', true);
   process.exit(1);
 }
@@ -19,19 +21,31 @@ if (!isUrl(require('./config.json').url)) {
   process.exit(1);
 }
 
-if (!isThere(__dirname + '/saves')) {
-  fs.mkdirSync(__dirname + '/saves');
+if (!isThere(path.resolve(__dirname, './saves/'))) {
+  fs.mkdirSync(path.resolve(__dirname, './saves/'));
   log('Saves directory created.', false);
 }
 
 var save = function () {
   var config = require('./config.json');
-  got(config.url + '/api/long', function (err, data) {
+  got(config.url + '/api?createdAt&content&attachments', function (err, data) {
        if (err) log('There was an error in getting data.', true);
        var name = moment().format('HH-mm-DD-MM-YYYY')  + '.json';
-       fs.writeFile(process.cwd() + '/saves/' + name, data, function (err) {
+       fs.writeFile(path.resolve(__dirname, './saves/',name), data, function (err) {
          if (err) log('There was an error in writing file.', true);
+
          log('Done! Generated in ' + name, false);
+         if (config.attachments) {
+           if (!isThere(path.resolve(__dirname, './saves/attachments/'))) {
+             fs.mkdirSync(path.resolve(__dirname, './saves/attachments/'));
+             log('Attachments directory created.', false);
+           }
+
+           downloadAttachments(config.url, function (err) {
+             if (err) log(err, true);
+             log('Attachments saved.', false);
+           });
+         }
        });
    });
 };
